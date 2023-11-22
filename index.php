0832e945460e53be97a47940546dfcb567b61246
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-11-22 16:51:43 +0100

    FIX - hotel parking

--- index.php
+++ index.php
@@ -48,11 +48,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
 
-
+    <div class='container'>
         <table class="table">
             <thead>
                 <tr>
@@ -65,32 +66,29 @@
             </thead>
             <tbody>
 
-            <?php 
-                foreach ($hotels as $hotel){
-            ?>
+            <?php foreach ($hotels as $hotel){ ?>
                 <tr>
                     <td> <?php echo $hotel['name'] ?> </td>
                     <td> <?php echo $hotel['description'] ?> </td>
                     <td>
-                        <?php 
-                            if ($hotel['parking'] === false) {
-                        ?>
+                        <?php if ($hotel['parking'] === false) { ?>
 
-                        <p>Nessun parcheggio disponibile</p>
+                        Nessun parcheggio disponibile
 
-                        <?php
-                            } else 
-                        ?>
+                        <?php } else { ?>
+
+                        Parcheggio disponbile
+
+                        <?php  } ?>
                     </td>
                     <td> <?php echo $hotel['vote'] ?> </td>
                     <td> <?php echo $hotel['distance_to_center'] ?> </td>
                 </tr> 
-            <?php
-            };
-            ?>
+            <?php }; ?>
+
             </tbody>
         </table>
-
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
